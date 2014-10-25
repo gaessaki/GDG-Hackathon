0b5417b452b979806e6408682477d2ec8bbb695e
Gaessaki <rsas23rd@yahoo.ca>
2014-10-25 13:30:09 -0400

    battery level

--- wear/src/main/java/com/brunstorm/awdronea/CenterActivity.java
+++ wear/src/main/java/com/brunstorm/awdronea/CenterActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
+import de.yadrone.base.ARDrone;
+import de.yadrone.base.IARDrone;
+import de.yadrone.base.navdata.BatteryListener;
 
 public class CenterActivity extends Activity {
 
@@ -20,5 +23,32 @@ public class CenterActivity extends Activity {
                 mTextView = (TextView) stub.findViewById(R.id.text);
             }
         });
+        IARDrone drone = null;
+        try
+        {
+            drone = new ARDrone();
+            drone.start();
+        }
+        catch (Exception exc)
+        {
+            exc.printStackTrace();
+        }
+        finally
+        {
+            if (drone != null)
+                drone.stop();
+            System.exit(0);
+        }
+        drone.getNavDataManager().addBatteryListener(new BatteryListener() {
+
+            public void batteryLevelChanged(int percentage)
+            {
+                //System.out.println("Battery: " + percentage + " %");
+            }
+
+            public void voltageChanged(int vbat_raw) { }
+        });
+
+
     }
 }
